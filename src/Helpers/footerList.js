9b1013703c947503dbb0905c60a887bb7b6a65c1
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-20 11:21:47 -0400

    refactor: add footerList helper
    An Array of objects added to improve the component
    
    PEIT-11 Footer

---
+++ src/Helpers/footerList.js
@@ -0,0 +1,57 @@
+export const footerList = [
+  {
+    id: crypto.randomUUID(),
+    title: "Information",
+    list: [
+      "About Us",
+      "About Zip",
+      "Privacy Policy",
+      "Search",
+      "Terms",
+      "Orders and Returns",
+      "Contact Us",
+      "Advanced Search",
+      "Newsletter Subscription",
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "PC Parts",
+    list: [
+      "CPUS",
+      "Add On Cards",
+      "Hard Drives (Internal)",
+      "Graphic Cards",
+      "Keyboards / Mice",
+      "Cases / Power Supplies / Cooling",
+      "RAM (Memory)",
+      "Software",
+      "Speakers / Headsets",
+      "Motherboards",
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Desktop PCs",
+    list: [
+      "Custom PCs",
+      "Servers",
+      "MSI All-In-One PCs",
+      "HP/Compaq PCs",
+      "ASUS PCs",
+      "Tecs PCs",
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Laptops",
+    list: [
+      "Everyday Use Notebooks",
+      "MSI Workstation Series",
+      "MSI Prestige Series",
+      "Tablets and Pads",
+      "Netbooks",
+      "Infinity Gaming Notebooks",
+    ],
+  },
+]
--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -1,1 +1,2 @@
+export * from "./footerList"
 export * from "./products"
