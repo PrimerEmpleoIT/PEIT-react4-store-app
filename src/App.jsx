963abb89ffa49b2c5119c1ffd81166d27dd91c41
Zoe Guzman <zoeguzman.ok@gmail.com>
2022-10-20 12:25:42 -0300

    añadi componente banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Layout } from "./Components"
+import Banner from "./Components/Banner/Banner"
 
 function App() {
   return (
     <>
-      <Layout></Layout>
+      
+      <Layout>
+        <Banner />
+      </Layout>
     </>
   )
 }
