e38491e48e941f7aa69c3d59c15d028d9db1c8cf
charlyls <carlosgbozzola@hotmail.com>
2022-10-21 18:50:49 -0300

    fix: cambio del menu en mobile

--- src/Components/Navbar/NavBarMobileScreen.jsx
+++ src/Components/Navbar/NavBarMobileScreen.jsx
@@ -1,74 +1,86 @@
 import { GiHamburgerMenu } from "react-icons/gi";
-import { GrCart } from "react-icons/gr";
+import { GrCart, GrFormNext } from "react-icons/gr";
 import { links } from "@/Helpers/navBarLinks";
 import {
-  Stack,
   Text,
   Flex,
   Image,
   Button,
   IconButton,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+  DrawerCloseButton,
+  Box,
 } from "@chakra-ui/react";
 import Logo from "./../../assets/navBar/Logo.svg";
 import ProfilePhoto from "./../../assets/navBar/ProfilePhoto.svg";
 
 function NavBarMobileScreen() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Stack borderBottom="1px" borderColor="gray" paddingY={2}>
-      <Menu>
-        {({ onClose }) => (
-          <>
-            <Flex justify="space-around" alignItems="center">
-              <MenuButton
-                as={IconButton}
-                aria-label="Options"
-                icon={<GiHamburgerMenu />}
-                variant="outline"
-              />
-              <Image src={Logo} />
+    <>
+      <Flex
+        justify="space-around"
+        alignItems="center"
+        borderBottom="1px"
+        borderColor="gray"
+        paddingY={2}
+      >
+        <Button
+          as={IconButton}
+          icon={<GiHamburgerMenu />}
+          variant="outline"
+          onClick={onOpen}
+        ></Button>
+        <Image src={Logo} />
 
-              <IconButton
-                aria-label="Cart"
-                size="lg"
-                icon={<GrCart />}
-                fontSize="20px"
-                Color="transparent"
-              />
-              <Button w="60px" h="36px" bgColor="transparent">
-                <Image src={ProfilePhoto} />
-              </Button>
-            </Flex>
-            <MenuList zIndex={10}>
-              <Flex alignItems="center" justify="space-between">
-                <Image src={Logo} h="30px" w="40px" mr={"120px"} />
-                <MenuItem onClick={() => onClose} w="0" mr="10px">
-                  {" "}
-                  <Text fontSize="25px">X</Text>
-                </MenuItem>
-              </Flex>
+        <IconButton
+          aria-label="Cart"
+          size="lg"
+          icon={<GrCart />}
+          fontSize="20px"
+          Color="transparent"
+        />
+        <Button w="60px" h="36px" bgColor="transparent">
+          <Image src={ProfilePhoto} />
+        </Button>
 
-              <MenuDivider />
-              {links.map((link) => {
-                return <MenuItem key={link.id}>{link.title}</MenuItem>;
-              })}
-              <Button
-                variant="outline"
-                colorScheme="blue"
-                borderRadius="66px"
-                ml={2}
-              >
-                Our deals
-              </Button>
-            </MenuList>
-          </>
-        )}
-      </Menu>
-    </Stack>
+        {/* <Box borderBottom="1px" borderColor="gray" paddingY={2}> */}
+      </Flex>
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen} gap={2}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <Flex direction="column">
+            <DrawerCloseButton fontSize="12px" mt="10px" />
+
+            <DrawerHeader borderBottomWidth="1px">
+              {" "}
+              <Image src={Logo} h="30px" w="40px" mr={"120px"} />
+            </DrawerHeader>
+          </Flex>
+          <DrawerBody>
+            {links.map((link) => {
+              return (
+                <Flex justify="space-between">
+                  <Text key={link.id} mb={2}>
+                    {link.title}
+                  </Text>
+                  <GrFormNext />
+                </Flex>
+              );
+            })}
+            <Button variant="outline" colorScheme="blue" borderRadius="66px">
+              Our deals
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 }
 
