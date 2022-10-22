88f38d93a0935d226178d530e7d08cffcaeef57c
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-22 12:07:33 -0400

    perf: Change format
    ESLint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { FollowUs, Layout } from './Components'
-import Banner from './Components/Banner/Banner'
-import NewProducts from './Components/Layout/NewProducts/NewProducts'
-import Sponsors from './Components/Sponsors/Sponsors'
+import { Banner, FollowUs, Layout, NewProducts, Sponsors } from './Components'
 
 function App() {
   return (
