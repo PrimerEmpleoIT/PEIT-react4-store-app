150b743462a7c3fbb9deecf114ca1789b903017b
Edisonmunoz13 <horacioem595>
2022-10-24 20:11:24 -0300

    resolviendo conflictos

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
-import { ChakraProvider } from '@chakra-ui/react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { theme } from './Theme'
-=======
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { theme } from "./Theme";
->>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
