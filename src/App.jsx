2154b7760634dc57459e864463487c7b80fd7e08
Edisonmunoz13 <horacioem595>
2022-10-24 20:09:59 -0300

    merge development

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 import {
   Ads,
   Banner,
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
+<<<<<<< Updated upstream
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { theme } from './Theme'
+=======
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { theme } from "./Theme";
+>>>>>>> Stashed changes
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
-)
+);
