4d685e94bd02e6fcdd43f5db7fcbd3a13bb35c4c
charlyls <carlosgbozzola@hotmail.com>
2022-10-24 19:53:13 -0300

    created mock data

---
+++ src/Helpers/storeProducts.js
@@ -0,0 +1,341 @@
+const storeProducts = [
+  {
+    id: 1,
+    name: "Custom Builds",
+    products: [
+      {
+        id: 1,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://i0.wp.com/www.inovamusicnet.com/wp-content/uploads/2022/02/81Y3A1TZakL.AC_SL600_.jpg?fit=600%2C367&ssl=1",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 1,
+      },
+      {
+        id: 2,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://i0.wp.com/www.inovamusicnet.com/wp-content/uploads/2022/02/81Y3A1TZakL.AC_SL600_.jpg?fit=600%2C367&ssl=1",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 1,
+      },
+      {
+        id: 3,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://i0.wp.com/www.inovamusicnet.com/wp-content/uploads/2022/02/81Y3A1TZakL.AC_SL600_.jpg?fit=600%2C367&ssl=1",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 1,
+      },
+      {
+        id: 4,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://i0.wp.com/www.inovamusicnet.com/wp-content/uploads/2022/02/81Y3A1TZakL.AC_SL600_.jpg?fit=600%2C367&ssl=1",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 1,
+      },
+      {
+        id: 5,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://i0.wp.com/www.inovamusicnet.com/wp-content/uploads/2022/02/81Y3A1TZakL.AC_SL600_.jpg?fit=600%2C367&ssl=1",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 1,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "MSI Laptops",
+    products: [
+      {
+        id: 1,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://asset.msi.com/resize/image/global/product/product_1652084089f186edc14f9d018b77a37b5aee4f5a5a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 2,
+      },
+      {
+        id: 2,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://asset.msi.com/resize/image/global/product/product_1652084089f186edc14f9d018b77a37b5aee4f5a5a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 2,
+      },
+      {
+        id: 3,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://asset.msi.com/resize/image/global/product/product_1652084089f186edc14f9d018b77a37b5aee4f5a5a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 2,
+      },
+      {
+        id: 4,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://asset.msi.com/resize/image/global/product/product_1652084089f186edc14f9d018b77a37b5aee4f5a5a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 2,
+      },
+      {
+        id: 5,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://asset.msi.com/resize/image/global/product/product_1652084089f186edc14f9d018b77a37b5aee4f5a5a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 2,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Desktops",
+    products: [
+      {
+        id: 1,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+      {
+        id: 1,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+      {
+        id: 2,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+      {
+        id: 3,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+      {
+        id: 4,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+      {
+        id: 5,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://front.dev.malditohard.com.ar/img/migration/GABINETE-GAMER-RAIDMAX-X616TBF.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 3,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Gaming Monitors",
+    products: [
+      {
+        id: 1,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://http2.mlstatic.com/D_NQ_NP_640887-MLA51095738517_082022-O.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 4,
+      },
+      {
+        id: 2,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://http2.mlstatic.com/D_NQ_NP_640887-MLA51095738517_082022-O.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 4,
+      },
+      {
+        id: 3,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://http2.mlstatic.com/D_NQ_NP_640887-MLA51095738517_082022-O.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 4,
+      },
+      {
+        id: 4,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://http2.mlstatic.com/D_NQ_NP_640887-MLA51095738517_082022-O.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 4,
+      },
+      {
+        id: 5,
+        title:
+          "EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...",
+        description:
+          "Windows 10 Home - MSI recommends Windows 11 Pro for business  FREE Upgrade to Windows 11 23.8 IPS Grade Panel LED Backlight (1920*1080 FHD)",
+        details:
+          "Featured MSI Super Charger technology that supports fast USB Sleep-and-Charge with auto-detection 178° wide viewing angle screen          ",
+        specs: null,
+        price: "33443221.00",
+        images:
+          "https://http2.mlstatic.com/D_NQ_NP_640887-MLA51095738517_082022-O.webp",
+        created_at: "2022-10-19T00:48:15.729856Z",
+        categories: 4,
+      },
+    ],
+  },
+];
