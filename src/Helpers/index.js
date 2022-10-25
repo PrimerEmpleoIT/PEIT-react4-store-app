e1f1dd5ebcdb3dd3cbc3d57fa782cd535a0f31b2
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-24 21:10:53 -0400

    chore: Update Barrels
    Update barrels and imports

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -1,6 +1,7 @@
-export * from "./NewData";
-export { default as followUs } from "./followUs";
-export * from "./footerList";
-export * from "./navBarLinks";
-export * from "./products";
-export * from "./testimonials";
+export * from './Benefits'
+export { default as followUs } from './followUs'
+export * from './footerList'
+export * from './navBarLinks'
+export * from './NewData'
+export * from './products'
+export * from './testimonials'
