06283527d80c99fbf5a6c74c0b787f1b7ddd5d1f
charlyls <carlosgbozzola@hotmail.com>
2022-10-24 21:19:21 -0300

    se agrego el store a la app

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import products from "./features/Products";
 // creacion del store  y en cada reducer para cada parte del estado del app
-export default configureStore({
+export const store = configureStore({
   reducer: {
     // creo un estado productos que van consumir la data que le pase el reducer products
     productos: products,
