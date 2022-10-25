e237d6ab355901ac4465ed5547672bee3e9e9ab0
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-25 11:39:15 -0400

    style: Change Banner style
    Banner width, height and showThumbs of carousel modified
    
    Style - Home

--- src/Components/Banner/ImageSlider.jsx
+++ src/Components/Banner/ImageSlider.jsx
@@ -4,9 +4,22 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
 const ImageSlider = ({ slides }) => {
   return (
-    <Carousel infiniteLoop showIndicators={false} showStatus={false}>
+    <Carousel
+      autoPlay={true}
+      infiniteLoop={true}
+      showIndicators={false}
+      showStatus={false}
+      dynamicHeight={true}
+      showThumbs={false}
+    >
       {slides.map((slide) => {
-        return <Image src={slide.image} height='auto' width='800px' />
+        return (
+          <Image
+            src={slide.image}
+            h={{ base: '20vh', lg: 'auto' }}
+            alt='banner-img'
+          />
+        )
       })}
     </Carousel>
   )
