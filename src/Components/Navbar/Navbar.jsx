ce205e0c3a3c20596cc1f683a1bce6ea5b0a84d2
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-24 21:52:24 -0400

    style: Change Navbar style
    Navbar tablet and desktop style changed
    
    PEIT-22 - Style Home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import Logo from '@/assets/navBar/Logo.svg'
 import ProfilePhoto from '@/assets/navBar/ProfilePhoto.svg'
 import { links } from '@/Helpers/navBarLinks'
-import { Button, Flex, Hide, IconButton, Image, Link } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Hide,
+  HStack,
+  IconButton,
+  Image,
+  Link,
+} from '@chakra-ui/react'
 import { GrCart } from 'react-icons/gr'
 import { NavBarMobileScreen } from '..'
 
@@ -10,43 +19,52 @@ function Navbar() {
     <>
       <Flex borderBottom='1px' borderColor='gray' justify='center'>
         <Flex
-          gap={{ base: '2', md: '3', lg: '6' }}
+          gap={{ base: '2', md: '3', lg: 10, xl: 16 }}
           h='92px'
           alignItems='center'
           display={['none', 'none', 'none', 'flex']}
+          px={2}
         >
           <Image src={Logo} h='30px' w='40px' />
-          {links.map((link) => {
-            return (
-              <Flex key={link.id}>
-                <Link
-                  fontSize={{ base: '12px', md: '12px', lg: '14px' }}
-                  fontWeight='600'
-                  fontFamily='Poppins'
-                >
-                  {link.title}
-                </Link>
-              </Flex>
-            )
-          })}
-          <Flex>
-            <Button variant='outline' colorScheme='blue' borderRadius='66px'>
-              Our deals
-            </Button>
-          </Flex>
-          <Flex>
-            <Flex alignItems='center' gap='2'>
-              <IconButton
-                aria-label='Cart'
-                size='lg'
-                icon={<GrCart />}
-                fontSize='20px'
-              />
-              <Button w='60px' h='36px' bgColor='transparent'>
-                <Image src={ProfilePhoto} />
+          <Flex gap={{ md: 2, lg: 8 }}>
+            {links.map((link) => {
+              return (
+                <HStack key={link.id}>
+                  <Link
+                    fontSize={{ base: '12px', md: '12px', lg: '14px' }}
+                    fontWeight='600'
+                    display={'flex'}
+                    align={'center'}
+                  >
+                    {link.title}
+                  </Link>
+                </HStack>
+              )
+            })}
+            <Box>
+              <Button
+                variant='outline'
+                colorScheme='blue'
+                fontSize={{ md: 'description', lg: 'general' }}
+                fontWeight={'semibold'}
+                w={{ md: '5rem', xl: '7.5rem' }}
+                borderRadius='66px'
+              >
+                Our deals
               </Button>
-            </Flex>
+            </Box>
           </Flex>
+          <HStack align='center'>
+            <IconButton
+              aria-label='Cart'
+              size='lg'
+              icon={<GrCart />}
+              fontSize='20px'
+            />
+            <Button w='60px' h='36px' bgColor='transparent'>
+              <Image src={ProfilePhoto} alt='profile-icon' />
+            </Button>
+          </HStack>
         </Flex>
       </Flex>
 
