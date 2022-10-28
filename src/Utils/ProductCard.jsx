d0453e5b3514c14fc362b94883c034e41dd7750b
mikejbp <74782928+mikejbp@users.noreply.github.com>
2022-10-28 16:00:26 -0400

    style: Change ProductCard
    ProductCard style changed

--- src/Utils/ProductCard.jsx
+++ src/Utils/ProductCard.jsx
@@ -1,19 +1,20 @@
-import { Box, Container, Flex, Heading, Image, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react'
 import { StarRating } from '.'
 
 function ProductCard({ id, stock, img, icon, description, oldPrice, price }) {
   return (
-    <Box key={id} fontFamily={'body'}>
+    <Box key={id} fontFamily={'body'} mx={{ base: '4', lg: 2, xl: 2 }}>
       <Box
         fontSize={'general'}
         bg='white'
         borderRadius={'3px'}
         cursor='pointer'
-        m={0}
+        mx={{ base: 2, md: '2' }}
         p={0}
+        w={{ lg: '90%', xl: '100%' }}
       >
         {stock === 'check availability' ? (
-          <Flex gap={1}>
+          <Flex gap={1} pb={2}>
             <Image w={3} src={icon} alt='unavailable' />
             <Heading
               as={'h4'}
@@ -25,7 +26,7 @@ function ProductCard({ id, stock, img, icon, description, oldPrice, price }) {
             </Heading>
           </Flex>
         ) : (
-          <Flex gap={1}>
+          <Flex gap={1} pb={2}>
             <Image w={3} src={icon} alt='available' />
             <Heading
               as={'h4'}
@@ -45,10 +46,10 @@ function ProductCard({ id, stock, img, icon, description, oldPrice, price }) {
         >
           {description}
         </Text>
-        <del> $ {oldPrice.toFixed(2)}</del>
+        <del> $ {oldPrice}</del>
         <Heading as={'h2'} fontSize={'paragraph'} fontWeight={'bold'}>
           {' '}
-          $ {price.toFixed(2)}
+          $ {price}
         </Heading>
       </Box>
     </Box>
